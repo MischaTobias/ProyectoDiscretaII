51dd46b8d0947a87109e08a590acdff95adfcb1d
Ale180820 <aylinneart@gmail.com>
2019-11-22 22:28:21 -0600

    Oa

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ using System.IO;
 namespace ProyectoDiscretaII
 {
     class NodoVertice
-    { 
+    {
         //nombre
         public string vertex;
         public List<string> conexiones;
@@ -58,12 +58,7 @@ namespace ProyectoDiscretaII
 
             try
             {
-                if (cantVertex1 == cantVertex2)
-                {
-                    string[,] matriz1 = new string[cantVertex1 + 1, cantVertex1 + 1];
-                    string[,] matriz2 = new string[cantVertex2 + 1, cantVertex2 + 1];
-                }
-                else
+                if (!(cantVertex1 == cantVertex2))
                 {
                     Console.WriteLine("Los grafos no son isomorfos, no tienen la misma cantidad de vértices");
                     Console.ReadKey();
@@ -90,15 +85,87 @@ namespace ProyectoDiscretaII
             grados1 = GenerarGrados(lista1, grados1);
             grados2 = GenerarGrados(lista2, grados2);
 
-            if (MismosGrados(grados1, grados2))
+            if (!(MismosGrados(grados1, grados2)))
             {
                 Console.Clear();
                 Console.WriteLine("No son isomorfos, no tienen la misma cantidad de vértices con el mismo grado");
                 Console.ReadKey();
                 Environment.Exit(0);
             }
+
+            //Obtenemos un arreglo de nodos ya ordenados
+            //   ↓↓
+            
+            var SortedArray = from Nodo in lista1 orderby Nodo.conexiones.Count select Nodo;
+            //La lista la reseteamos
+            lista1 = new List<NodoVertice>();
+            //para cada elemento en el SortedArr
+            //   ingresarlo en la lista que ahora esta vacia
+            foreach (var NodoActual in SortedArray)
+            {
+                lista1.Add(NodoActual);
+            }
+            //ya tenemos la lista ordenada ;)
+
+            //var SortedArray2 = from Nodo in lista2 orderby Nodo.conexiones.Count select Nodo;
+            //lista2 = new List<NodoVertice>();
+            ////para cada elemento en el SortedArr
+            ////   ingresarlo en la lista que ahora esta vacia
+            //foreach (var NodoActual in SortedArray2)
+            //{
+            //    lista2.Add(NodoActual);
+            //}
+            string[,] matriz1 = new string[cantVertex1 + 1, cantVertex1 + 1];
+            string[,] matriz2 = new string[cantVertex2 + 1, cantVertex2 + 1];
+
+            for (int i = 0; i < lista1.Count; i++)
+            {
+                 matriz1[0, i + 1] =  lista1[i].vertex;
+                 matriz1[i + 1, 0] =  lista1[i].vertex;
+            }
+
+
+            for (int j = 0; j < lista2.Count; j++)
+            {
+                  matriz2[0, j + 1] =  lista2[j].vertex;
+                  matriz2[j + 1, 0] =  lista2[j].vertex;
+            }
+
+            for (int i = 0; i < lista1.Count; i++)
+            {
+                for (int j = 0; j < lista1.Count; j++)
+                {
+                    matriz1[i + 1, j + 1] = Existe(matriz1[0, j], matriz1[i, 0], lista1);
+                }
+            }
+
+            for (int i = 0; i < lista2.Count; i++)
+            {
+                for (int j = 0; j < lista2.Count; j++)
+                {
+                    matriz2[i + 1, j + 1] = Existe(matriz2[0, j], matriz2[i, 0], lista2);
+                }
+            }
+
         }
 
+        static string Existe(string vfila, string vcolumna, List<NodoVertice> lista)
+        {
+            for (int i = 0; i < lista.Count; i++)
+            {
+                if (lista[i].vertex == vcolumna)
+                {
+                    for (int j = 0; j < lista[i].conexiones.Count; j++)
+                    {
+                        if (lista[i].conexiones[j] == vfila)
+                        {
+                            return "1";
+                        }
+                    }
+                }
+            }
+            return "0";
+        }
         static bool MismosGrados(List<int> grados1, List<int> grados2)
         {
             for (int i = 0; i < grados1.Count; i++)
@@ -241,5 +308,16 @@ namespace ProyectoDiscretaII
             return false;
         }
         //Función que comprueba si 2 grafos tenen la misma cantidad de vértices con base en el primer dato en los array
+        //static string FuncionIsomorfismo(string[,] matriz1, string[,] matriz2, int cantVertex1, int cantVertex2)
+        //{
+        //    for(int i = 0; i<cantVertex1 + 1; i++)
+        //    {
+        //        for (int j = 0; j < cantVertex1 + 1; j++)
+        //        {
+
+        //        }
+        //    }
+        //    return " ";
+        //}
     }
 }
